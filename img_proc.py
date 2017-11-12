e930d2012ee42c126cec24b7a92d938d786cccc7
toastom <30571257+toastom@users.noreply.github.com>
2017-11-12 17:30:25 -0500

    Add files via upload

--- img_proc.py
+++ img_proc.py
@@ -1,54 +1,62 @@
 from Tkinter import *
-import Tkinter, Tkconstants, tkFileDialog
+import Tkinter, Tkconstants, tkFileDialog, tkMessageBox, tkSimpleDialog
 from PIL import Image
 from PIL import ImageFilter
 
 
 class App:
     def __init__(self, master):
-        self.frame = Frame()
+        self.frame = Frame(master, width=500, height=20)
         self.frame.pack()
 
-        #self.photo_label = Label(master, image=self.image)
-        #self.photo_label.pack(side=TOP)
+        self.quit_button = Button(master, text="Quit", command=master.destroy)
+        self.quit_button.pack(side=BOTTOM)
+        
 
         self.my_menu = Menu(master)
         master.config(menu=self.my_menu)
 
         self.sub_menu = Menu(self.my_menu)
-        self.my_menu.add_cascade(label="File", menu=self.sub_menu)
-        self.sub_menu.add_command(label="Add Image", command=self.add_image)
+        self.my_menu.add_cascade(label="Edit Image", menu=self.sub_menu)
+        self.sub_menu.add_command(label="Convert to Black/White", command=self.convert_b_w)
 
-        self.toolbar = Frame(master)
-        self.convert = Button(self.toolbar, text="Convert to Black/White", command=self.convert_b_w)
-        self.convert.pack(side=LEFT)
+        self.sub_menu.add_command(label="Rotate Image", command=self.rotate_image)
 
-        #self.preview = Button(self.toolbar, text="Image Preview", command=)
-
-        self.toolbar.pack(side=TOP)
-
-    def add_image(self):
-        self.filename = tkFileDialog.askopenfilename(initialdir="C:\Python\my_scripts",
-                                                     title="Select Image",
-                                                     filetypes=(("jpeg files", "*jpg"),
-                                                                ("all files", "*.*")))
-        self.image = Image.open(self.filename)
-        
-        
-        print("Added {} image".format(self.filename))
+        #self.toolbar = Frame(master)
+        #self.toolbar.pack(side=TOP)
 
     def convert_b_w(self):
+        #File dialog to let the user choose which image they want
         self.filename = tkFileDialog.askopenfilename(initialdir="C:\Python\my_scripts",
                                                      title="Select Image",
                                                      filetypes=(("jpeg files", "*jpg"),
                                                                 ("all files", "*.*")))
         
-        self.image = Image.open(self.filename)
-        self.image.convert(mode='L')
-        print("Converted to Black and White")
+        self.image = Image.open(self.filename) #Opens image the user chose
+        self.image.convert(mode='L').save(self.filename) #Converts the image and saves
 
+        tkMessageBox.showinfo("Saved", "Image saved") #Dialog to show we saved
+
+
+    def rotate_image(self):
+        #File choosing dialog
+        self.filename = tkFileDialog.askopenfilename(initialdir="C:\Python\my_scripts",
+                                                     title="Select Image",
+                                                     filetypes=(("jpeg files", "*jpg"),
+                                                                ("all files", "*.*")))
+        self.image = Image.open(self.filename) #Opens image
+
+        self.rotation_value = tkSimpleDialog.askinteger("Rotate", "Rotate Degrees Clockwise",
+                                minvalue=0, maxvalue=360)
+        #Default with Tk is to rotate counter-clockwise, so we have to make it negative
+        #to flip clockwise
+        self.image.rotate(self.rotation_value * -1).save(self.filename)
+
+        tkMessageBox.showinfo("Saved", "Image saved") #User feedback for saving image
+
+        
+       
 root = Tk()
-
 app = App(root)
-
+root.title("Image Manipulator")
 root.mainloop()
